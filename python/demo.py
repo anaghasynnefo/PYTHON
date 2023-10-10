5e6d6f269759a114e8cbe5ac92352ea9371d6391
anaghasynnefo <136800420+anaghasynnefo@users.noreply.github.com>
2023-10-10 09:14:29 +0530

    Add files via upload

---
+++ python/demo.py
@@ -0,0 +1,6 @@
+print('hello')
+a=10
+b=3
+print(type(a))
+a={}
+print(type(a))
---
+++ python/list.py
@@ -0,0 +1,13 @@
+list=[1,3,5,7,9,11,13,4,6,8]
+# print(list)
+# list.append(5)
+# print(list)
+# print(type (list))
+# # print(list[3])
+#  list[5]=12
+# print(list)
+# list.extend([2,3,4])
+# print (list)
+# list.insert(4,10)
+# print(list)
+print(list.count(2)) 
