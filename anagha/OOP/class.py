8ca606f01368569a478cb18125cb8a306ba3298e
anaghasynnefo <136800420+anaghasynnefo@users.noreply.github.com>
2023-11-02 09:24:04 +0530

    Add files via upload

--- anagha/OOP/class.py
+++ anagha/OOP/class.py
@@ -18,13 +18,39 @@
 #         car=vehicle()
 #         car.start(5,3)
 
+# class vehicle:
+#     wheel=4
+#     door=4
+
+#     def start(self,type):
+#         print("type,engine start")
+
+#         car=vehicle()
+#         car.start("engine stop")
+
+
+# class vehicle:
+#     color="red"
+#     def show(self):
+#         print("color is",self.color)
+# car=vehicle()
+# car.show()
+
+# class vehicle:
+#     def setcolor(self,color):
+#         self.color=color
+#     def show(self):
+#         print("color is",self.color)
+# car=vehicle() 
+# car.setcolor("meroon")       
+# car.show()
+
+
 class vehicle:
-    wheel=4
-    door=4
-
-    def start(self,type):
-        print("type,engine start")
-
-        car=vehicle()
-        car.start("engine stop")
-
+    def setcolor(self,seat):
+        self.seat=seat
+    def show(self):
+        print("number of seat  is",self.seat)
+car=vehicle() 
+car.setcolor("5")       
+car.show()
